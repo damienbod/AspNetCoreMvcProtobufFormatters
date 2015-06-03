fc7403a5d4535efd3eba47e1ad60e9ee14a4b4cd
damienbod <damien_bod@hotmail.com>
2015-06-03 21:50:09 +0200

    clean up

--- src/AspNetMvc6Protobuf/Startup.cs
+++ src/AspNetMvc6Protobuf/Startup.cs
@@ -24,9 +24,7 @@ namespace AspNetMvc6Protobuf
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-            // Uncomment the following line to add Web API services which makes it easier to port Web API 2 controllers.
-            // You will also need to add the Microsoft.AspNet.Mvc.WebApiCompatShim package to the 'dependencies' section of project.json.
-            // services.AddWebApiConventions();
+
             services.ConfigureMvc(options =>
             {
                 options.InputFormatters.Add(new ProtobufInputFormatter());
